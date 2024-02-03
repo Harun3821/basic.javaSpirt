59721829b892083a94c4b2c4fb642a9fc6e171d4
Harun3821 <harun4382183@gmail.com>
2024-02-03 15:48:57 +0600

    first commit function diclersion & function call

--- fun.js
+++ fun.js
@@ -11,3 +11,39 @@ let = 56;
 
 // lightof();
 
+// parameter function -------//
+
+// 3--> 3*3 = 9
+// 4--> 4*4 = 16
+// 5--> 5*5 = 25 
+// 6--> 6*6 = 36
+// 8--> 8*8 = 64
+
+// function square (number){
+//     console.log('valeu of the parameter', number)
+//     let square = number * number
+//     console.log('square of the numbere', square);
+// }
+
+
+// square(5)
+// square(12)
+// square(50)
+// square(122)
+
+function add (num1, num2){
+    let sum = num1 + num2;
+    console.log(sum);
+}
+
+add(5, 71)
+add(50, 51)
+
+function addAll (a, b, c, d, e){
+    let summery = a + b + c + d + e;
+    console.log(a, b, c, d, e)
+    console.log(summery);
+}
+
+addAll(9, 5, 8, 3, );
+addAll(9, 5, 8, 3, 2);

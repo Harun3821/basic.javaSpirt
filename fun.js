6062845b694c77e12d4f205b11a402b843c92937
Harun3821 <harun4382183@gmail.com>
2024-02-03 15:27:20 +0600

    first commit functiondiclerson & call

---
+++ fun.js
@@ -0,0 +1,13 @@
+let = 56;
+// function diclerson-------//
+
+// function lightof(){
+//     console.log('ajkey amara pincik kahabo');
+//     console.log('amar kacha taka nani keno');
+//     console.log('tu akta balkama amare ki korbi');
+// }
+
+// call the function-------//
+
+// lightof();
+

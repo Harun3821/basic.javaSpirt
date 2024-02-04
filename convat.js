729f33f33aa8bb4939f8bc30a3495ffe14971e3d
Harun3821 <harun4382183@gmail.com>
2024-02-04 17:14:39 +0600

    first commit convat and math and Array ducpitat

---
+++ convat.js
@@ -0,0 +1,66 @@
+// number convat----//
+// let a = 6;
+// let b = 9;
+// console.log(a, b);
+
+// // a = b;
+// // b = a;
+
+
+// const temp = a;
+// a = b;
+// b = temp;
+// console.log(a, b);
+
+// math --------//
+
+// const Math = {
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+
+// console.log(Math.min);
+
+// const min = Math.min(34, 78, 9, 44, 77, 90);
+// console.log(min);
+// const max = Math.max(34, 78, 9, 44, 77, 90, 999);
+// console.log(max);
+
+// console.log(Math.PI);
+// console.log(Math.abs(90-100))
+// console.log(Math.round(9.35))
+// console.log(Math.floor(21.99995))
+// console.log(Math.ceil(21.00001))
+// console.log(Math.random())
+// console.log(Math.random()*10)
+
+// const rant = Math.round(Math.random()*10)
+// console.log(rant);
+
+
+
+// -----Array has some ducpitat eliments----//
+
+// []
+
+// const birnekhor = ['harun', 'rhaim', 'kharm', 'abul', 'harun',' rhaim']
+// const number = [4, 6, 7, 8, 9, 4, 12, 7, 12];
+
+// function noDoblekeit(Array){
+//     const unique = [];
+//     for(const item of Array){
+//     if(unique.includes(item) === false){
+//         unique.push(item);
+//     }
+   
+//     }
+//     return unique;
+// }
+
+
+
+
+
+// const uniqueArray = noDoblekeit(number);
+// console.log(uniqueArray);
--- fun.js
+++ fun.js
@@ -1,49 +1,69 @@
-// let = 56;
-// function diclerson-------//
+// // let = 56;
+// // function diclerson-------//
 
-function lightof(){
-    console.log('ajkey amara pincik kahabo');
-    console.log('amar kacha taka nani keno');
-    console.log('tu akta balkama amare ki korbi');
-}
+// function lightof(){
+//     console.log('ajkey amara pincik kahabo');
+//     console.log('amar kacha taka nani keno');
+//     console.log('tu akta balkama amare ki korbi');
+// }
 
-// call the function-------//
+// // call the function-------//
 
-// lightof();
+// // lightof();
 
-// parameter function -------//
+// // parameter function -------//
 
-// 3--> 3*3 = 9
-// 4--> 4*4 = 16
-// 5--> 5*5 = 25 
-// 6--> 6*6 = 36
-// 8--> 8*8 = 64
+// // 3--> 3*3 = 9
+// // 4--> 4*4 = 16
+// // 5--> 5*5 = 25 
+// // 6--> 6*6 = 36
+// // 8--> 8*8 = 64
 
-function square (number){
-    console.log('valeu of the parameter', number)
-    let square = number * number
-    console.log('square of the numbere', square);
-}
+// function square (number){
+//     console.log('valeu of the parameter', number)
+//     let square = number * number
+//     console.log('square of the numbere', square);
+// }
 
 
-square(5)
-square(12)
-square(50)
-square(122)
+// square(5)
+// square(12)
+// square(50)
+// square(122)
 
-function add (num1, num2){
-    let sum = num1 + num2;
-    console.log(sum);
-}
+// function add (num1, num2){
+//     let sum = num1 + num2;
+//     console.log(sum);
+// }
 
-add(5, 71)
-add(50, 51)
+// add(5, 71)
+// add(50, 51)
 
-function addAll (a, b, c, d, e){
-    let summery = a + b + c + d + e;
-    console.log(a, b, c, d, e)
-    console.log(summery);
-}
+// function addAll (a, b, c, d, e){
+//     let summery = a + b + c + d + e;
+//     console.log(a, b, c, d, e)
+//     console.log(summery);
+// }
 
-addAll(9, 5, 8, 3, );
-addAll(9, 5, 8, 3, 2);
+// addAll(9, 5, 8, 3, );
+// addAll(9, 5, 8, 3, 2);
+
+// function doubelIt (num){
+//   let doubelIt = num * 2;
+//   console.log(doubelIt, num);
+// }
+
+// doubelIt(22);
+// doubelIt(15);
+// doubelIt(88);
+
+// function differecence (num1, num2){
+//  let diff = num1 - num2;
+//  console.log(diff);
+// }
+
+// differecence(11, 9);
+
+// let fatheragg = 80;
+// let sunagg = 30;
+// differecence(fatheragg, sunagg);
